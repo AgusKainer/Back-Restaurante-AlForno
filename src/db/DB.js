f00b1e1cf2857352e8f6ebd7331493def01edc7c
agus kainer <puchidev98@gmail.com>
2025-12-06 14:10:06 -0300

    probando en render

--- src/db/DB.js
+++ src/db/DB.js
@@ -1,13 +1,3 @@
-// const { Sequelize } = require("sequelize");
-// const { DB_HOST, DB_NAME, DB_PASS, DB_USER } = require("../utils/config");
-
-// const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-//   host: DB_HOST,
-//   dialect: "mysql",
-// });
-
-// module.exports = db;
-
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize(
@@ -19,6 +9,12 @@ const db = new Sequelize(
     port: process.env.MYSQLPORT,
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-const app = require("./app");
-const path = require("path");
 require("dotenv").config();
+
+const app = require("./app");
 const db = require("./db/DB");
 
 const PORT = process.env.PORT || 3000;
+
 const server = async () => {
   try {
-    console.log("puerto: ", PORT);
+    console.log("PUERTO:", PORT);
 
+    await db.authenticate();
     await db.sync();
-    console.log("db conectada");
+    console.log("DB CONECTADA");
+
     app.listen(PORT, "0.0.0.0", () => {
       console.log("SERVER LEVANTADO EN PUERTO:", PORT);
     });
   } catch (error) {
-    console.log("ERROR AL LAVANTAR EL SERVIDOR", error);
+    console.log("ERROR AL LEVANTAR EL SERVIDOR:", error);
   }
 };
 
